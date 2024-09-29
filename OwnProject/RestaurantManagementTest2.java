80f7cd733d199e201679f7499faab9fa558f4944
ishfak-akbar <135794512+ishfak-akbar@users.noreply.github.com>
2024-09-29 19:41:15 +0600

    RestaurantManagementTest2.java

--- OwnProject/RestaurantManagementTest2.java
+++ OwnProject/RestaurantManagementTest2.java
@@ -1,19 +1,47 @@
 
 package OwnProject;
 
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class RestaurantManagementTest2{
     public static void main(String[] args) {
 
         RestaurantManagement2 restaurant = new RestaurantManagement2();
-        
         restaurant.credits(5000);
+
+        JPanel loginPanel = new JPanel();
+        loginPanel.setLayout(new BoxLayout(loginPanel, BoxLayout.Y_AXIS));
+
         
-        String email = JOptionPane.showInputDialog(null,"Enter Email : ");
-        String password = JOptionPane.showInputDialog(null,"Enter Password : ");
-             
-        restaurant.loginPage(email, password);
+        JLabel emailLabel = new JLabel("Enter Email: ");
+        JTextField emailField = new JTextField(20);
+        loginPanel.add(emailLabel);
+        loginPanel.add(emailField);
+
+        // Add label and text field for password (using JPasswordField for password masking)
+        JLabel passwordLabel = new JLabel("Enter Password: ");
+        JPasswordField passwordField = new JPasswordField(20);
+        // Set echoChar to mask password characters with an asterisk (*)
+        passwordField.setEchoChar('*');
+        loginPanel.add(passwordLabel);
+        loginPanel.add(passwordField);
+
+        // Show the login dialog and retrieve user input
+        int result = JOptionPane.showConfirmDialog(null, loginPanel, "Login", JOptionPane.OK_CANCEL_OPTION);
+
+        if (result == JOptionPane.OK_OPTION){
+            String email = emailField.getText();
+            String password = new String(passwordField.getPassword()); // Convert char array to String
+            restaurant.loginPage(email, password);
+        } 
+        else {
+            System.exit(0); // Exit if user cancels login
+        }
         
     
 
